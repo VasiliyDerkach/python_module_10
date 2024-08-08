cfcdad77b8e0ad52e5ac7f3496ba14d2e7b94ddd
Vasiliy <v_derkach@inbox.ru>
2024-08-08 15:21:45 +0500

    fin

--- module_11_1.py
+++ module_11_1.py
@@ -4,8 +4,17 @@ import numpy
 url = 'https://midural.ru/100034/100089/mu_leaders/'
 resp = requests.get(url)
 print(resp.content)
+print(resp.headers)
 html1 = pandas.read_html(url)
 print(html1)
+excl = pandas.DataFrame({'Cat': ['Pers','Scotish','BRIT']
+                         ,'LENGTH': [250,200,300]
+                         ,'Color':['White','Grey','Many']})
+
+excl.to_excel('cats.xlsx')
 alfa=numpy.array([55,0,15,4,3,15,3,1])
-print(numpy.unique_counts(alfa))
-#получаем массив из уникальных элементов заданного массива и для каждого уникального элемента количество таких элементов в заданном масииве
+un, count_un =numpy.unique_counts(alfa)
+#получаем массив из уникальных элементов заданного массива и для каждого уникального элемента количество таких элементов в заданном масиивеdt = numpy.dot(un,count_un) #скалярное произведение двух массивов
+print(un , count_un)
+dt = numpy.dot(un, count_un) #скалярное произвдение двух массивов
+print(dt)
