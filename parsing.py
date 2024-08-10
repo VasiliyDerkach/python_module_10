d6ab7d976548f7f3df44f4b75231464ebafb3ba1
Vasiliy <v_derkach@inbox.ru>
2024-08-10 20:56:47 +0500

    cart of buss2

--- parsing.py
+++ parsing.py
@@ -9,6 +9,8 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 if __name__ == '__main__':
     #option = Options()
     #option.add_argument("--disable-infobars")
@@ -30,11 +32,38 @@ if __name__ == '__main__':
         #lst = driver.find_element(By.ID,cn)
         lst1 = driver.find_element(By.ID,cn)
         lst1.click()
-        tc = driver.find_element(By.ID,tx[0])
-        while not tc.is_displayed():
-            pass
-        htm = driver.page_source
-        htm1 = pd.read_html(htm, match=tx[1])
-        #print(lst1.text)
-        print(tc.text)
-        print(htm1)
+        #tc = driver.find_element(By.ID,tx[0])
+        try:
+            tc = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, tx[0]))
+            )
+            print(tc.text)
+            htm = tc.find_element(By.ID, "tablcont")
+            #print(htm.text)
+            rows = htm.find_elements(by=By.TAG_NAME, value='tr')
+            # подсчет количества столбцов
+            cols = htm.find_elements(by=By.TAG_NAME, value='td')
+            print(len(rows),len(cols))
+
+            #htm = tc.find_element(By.ID,'tablcont')
+            #htm = driver.page_source
+            #htm1 = pd.read_html(htm, match=tx[1])
+            #print(htm.text)
+        except:
+            print('не загрузилось')
+
+        #time.sleep(5)
+        # while not tc.is_displayed():
+        #     pass
+
+        # #print(lst1.text)
+
+        # with open('delo.txt',mode='w',encoding='utf-8') as f:
+        #     f.write(tc.text)
+
+    driver.quit()
+"""    driver = Chrome()
+    url = 'https://chkalovsky--svd.sudrf.ru/modules.php?name=sud_delo&srv_num=1&name_op=r&delo_id=1540005&case_type=0&new=0&G1_PARTS__NAMESS=&g1_case__CASE_NUMBERSS=&g1_case__JUDICIAL_UIDSS=&captcha=65736&captchaid=16pihpfpksoa7ncagilpqli7e2&delo_table=g1_case&g1_case__ENTRY_DATE1D=&g1_case__ENTRY_DATE2D=&lawbookarticles%5B%5D=%D1%EF%EE%F0%FB%2C+%E2%EE%E7%ED%E8%EA%E0%FE%F9%E8%E5+%E8%E7+%F2%F0%F3%E4%EE%E2%FB%F5+%EE%F2%ED%EE%F8%E5%ED%E8%E9&G1_CASE__JUDGE=&g1_case__RESULT_DATE1D=&g1_case__RESULT_DATE2D=&G1_CASE__RESULT=&G1_CASE__BUILDING_ID=&G1_CASE__COURT_STRUCT=&G1_EVENT__EVENT_NAME=&G1_EVENT__EVENT_DATEDD=&G1_PARTS__PARTS_TYPE=&G1_PARTS__INN_STRSS=&G1_PARTS__KPP_STRSS=&G1_PARTS__OGRN_STRSS=&G1_PARTS__OGRNIP_STRSS=&G1_RKN_ACCESS_RESTRICTION__RKN_REASON=&g1_rkn_access_restriction__RKN_RESTRICT_URLSS=&g1_requirement__ACCESSION_DATE1D=&g1_requirement__ACCESSION_DATE2D=&G1_REQUIREMENT__CATEGORY=&g1_requirement__ESSENCESS=&g1_requirement__JOIN_END_DATE1D=&g1_requirement__JOIN_END_DATE2D=&G1_REQUIREMENT__PUBLICATION_ID=&G1_DOCUMENT__PUBL_DATE1D=&G1_DOCUMENT__PUBL_DATE2D=&G1_CASE__VALIDITY_DATE1D=&G1_CASE__VALIDITY_DATE2D=&G1_ORDER_INFO__ORDER_DATE1D=&G1_ORDER_INFO__ORDER_DATE2D=&G1_ORDER_INFO__ORDER_NUMSS=&G1_ORDER_INFO__EXTERNALKEYSS=&G1_ORDER_INFO__STATE_ID=&G1_ORDER_INFO__RECIP_ID=&Submit=%CD%E0%E9%F2%E8'
+    driver.get(url)
+    time.sleep(5)
+    driver.quit()"""
