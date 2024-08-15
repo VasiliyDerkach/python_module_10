e73d57fbe2fe1e701231033dd3697dfe5e2aa949
Vasiliy <v_derkach@inbox.ru>
2024-08-15 19:57:28 +0500

    +доп. закладки

--- court_parsing.py
+++ court_parsing.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     #option.add_argument("--disable-infobars")
     driver = Chrome()
     #driver = Chrome(executable_path='C:/distrib/chromedriver.exe',options=option)
-    url = 'https://chkalovsky--svd.sudrf.ru/modules.php?name=sud_delo&srv_num=1&name_op=case&case_id=436119774&case_uid=2854f329-d474-4336-8775-87b3fc142d77&delo_id=1540005'
+    url = 'https://asbestovsky--svd.sudrf.ru/modules.php?name=sud_delo&srv_num=1&name_op=case&case_id=390794311&case_uid=a4534ef3-49d4-46c0-a04b-fc9e2ab5a9b4&delo_id=1540005'
     #url = 'https://oktiabrsky--svd.sudrf.ru/modules.php?name=sud_delo&srv_num=1&name_op=r&delo_id=1540005&case_type=0&new=0&G1_PARTS__NAMESS=&g1_case__CASE_NUMBERSS=&g1_case__JUDICIAL_UIDSS=&captcha=36960&captchaid=lvl06vonmve1j68ilun3aeqph0&delo_table=g1_case&g1_case__ENTRY_DATE1D=&g1_case__ENTRY_DATE2D=&lawbookarticles%5B%5D=%D1%EF%EE%F0%FB%2C+%E2%EE%E7%ED%E8%EA%E0%FE%F9%E8%E5+%E8%E7+%F2%F0%F3%E4%EE%E2%FB%F5+%EE%F2%ED%EE%F8%E5%ED%E8%E9&G1_CASE__JUDGE=&g1_case__RESULT_DATE1D=&g1_case__RESULT_DATE2D=&G1_CASE__RESULT=&G1_CASE__BUILDING_ID=&G1_CASE__COURT_STRUCT=&G1_EVENT__EVENT_NAME=&G1_EVENT__EVENT_DATEDD=&G1_PARTS__PARTS_TYPE=&G1_PARTS__INN_STRSS=&G1_PARTS__KPP_STRSS=&G1_PARTS__OGRN_STRSS=&G1_PARTS__OGRNIP_STRSS=&G1_RKN_ACCESS_RESTRICTION__RKN_REASON=&g1_rkn_access_restriction__RKN_RESTRICT_URLSS=&g1_requirement__ACCESSION_DATE1D=&g1_requirement__ACCESSION_DATE2D=&G1_REQUIREMENT__CATEGORY=&g1_requirement__ESSENCESS=&g1_requirement__JOIN_END_DATE1D=&g1_requirement__JOIN_END_DATE2D=&G1_REQUIREMENT__PUBLICATION_ID=&G1_DOCUMENT__PUBL_DATE1D=&G1_DOCUMENT__PUBL_DATE2D=&G1_CASE__VALIDITY_DATE1D=&G1_CASE__VALIDITY_DATE2D=&G1_ORDER_INFO__ORDER_DATE1D=&G1_ORDER_INFO__ORDER_DATE2D=&G1_ORDER_INFO__ORDER_NUMSS=&G1_ORDER_INFO__EXTERNALKEYSS=&G1_ORDER_INFO__STATE_ID=&G1_ORDER_INFO__RECIP_ID=&Submit=%CD%E0%E9%F2%E8'
     driver.get(url)
 
@@ -56,11 +56,12 @@ if __name__ == '__main__':
                 tc = WebDriverWait(driver, 15).until(
                     EC.presence_of_element_located((By.ID, tx[0]))
                 )
-
+                # предусмотреть чтение заголовков таблиц
                 if tx[0]==tx[1]:
                     htm = tc
                 else:
                     htm = tc.find_element(By.ID, tx[1])
+                    #предусмотреть случаи (реально есть), когда элементов tx[1] несколько на закладке (несколько таблиц)
 
                 rows = htm.find_elements(by=By.TAG_NAME, value='tr')
                 # подсчет количества столбцов
